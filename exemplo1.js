0e2aad8b1380ec58d1bc15861d4a01cb4bec146c
Alessandro Goncalves <senac.alessandro@gmail.com>
2024-09-27 20:44:26 -0300

    exemplos aula 2

---
+++ exemplo1.js
@@ -0,0 +1,7 @@
+// Exercicio exemplo 1
+
+let nome = 'Alessandro'
+let sobrenome = 'Gonçalves'
+let idade = 30 //usando const
+let estudante = true
+console.log(nome, sobrenome, idade, estudante)
---
+++ exemplo2.js
@@ -0,0 +1,7 @@
+let nome = prompt('Qual seu nome?')
+let idade = prompt('Qual sua idade?')
+
+console.log('variavel "nome" é ', nome, ' seu tipo é ', typeof nome)
+console.log('variavel \'idade\' é ', idade, ' seu tipo é ', typeof +idade)
+
+
