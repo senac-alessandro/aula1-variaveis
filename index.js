c9ec434d8dbc01bf401c7eb1f7c2474cbac5b9c8
Alessandro Goncalves <senac.alessandro@gmail.com>
2024-09-26 21:42:11 -0300

    aprendendo comando basicos em javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+// isso é um comentario de uma linha
+/*
+Esse é o meu primeiro script JS
+Estou aprendendo sobre comentar comentar o código
+*/
+
+let nome = prompt('Qual é o seu nome?')
+alert('Seja bem vindo ' + nome + '!')
+
+console.log('imprimindo no console do navegador')
